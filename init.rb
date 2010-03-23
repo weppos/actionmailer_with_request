4a614b3a74d4e9bc6a0fd591e406a5e501182d82
Simone Carletti <weppos@weppos.net>
2010-03-23 12:17:32 +0100

    Fixed uninitialized constant ActionMailerWithRequest::MailerMonkeyPatch error (closes #1)

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 require 'actionmailer_with_request'
 
 ActionController::Base.send :include, ActionMailerWithRequest::ControllerMixin
-ActionMailer::Base.send :include, ActionMailerWithRequest::MailerMonkeyPatch
+ActionMailer::Base.send :include, ActionMailerWithRequest::MailerDefaultUrlOptions
 
 Rails.logger.info("** ActionMailerWithRequest: initialized properly") if defined?(Rails)
