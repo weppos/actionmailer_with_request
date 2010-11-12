3e24282289127be0992170053fd62a0056ae062e
Eric Anderson <eric@pixelwareinc.com>
2010-11-12 11:25:59 -0500

    Update to Rails 3 plugin/gem init. Gem support. Add default :to/:from support

--- init.rb
+++
@@ -1,6 +0,0 @@
-require 'actionmailer_with_request'
-
-ActionController::Base.send :include, ActionMailerWithRequest::ControllerMixin
-ActionMailer::Base.send :include, ActionMailerWithRequest::MailerDefaultUrlOptions
-
-Rails.logger.info("** ActionMailerWithRequest: initialized properly") if defined?(Rails)
--- lib/actionmailer_with_request.rb
+++ lib/actionmailer_with_request.rb
@@ -14,10 +14,10 @@ module ActionMailerWithRequest
 
   end
 
-  module MailerDefaultUrlOptions
+  module MailerDefaults
 
     def self.included(base)
-      base.class_eval do
+      base.singleton_class.class_eval do
         def default_url_options_with_current_request
           host = Thread.current[:request].try(:host)
           port = Thread.current[:request].try(:port)
@@ -27,7 +27,22 @@ module ActionMailerWithRequest
           default_url_options_without_current_request.merge(default)
         end
         alias_method_chain :default_url_options, :current_request
+
+        def default_with_current_request
+          defaults = {}
+          defaults[:to] = "admin@#{default_url_options[:host]}"
+          defaults[:from] = "no-reply@#{default_url_options[:host]}"
+          default_without_current_request.merge(defaults)
+        end
+        alias_method_chain :default, :current_request
       end
+    end
+  end
+
+  class Railtie < Rails::Railtie
+    initializer 'actionmailer.with_request.mixin' do
+      ActionController::Base.send :include, ActionMailerWithRequest::ControllerMixin
+      ActionMailer::Base.send :include, ActionMailerWithRequest::MailerDefaults
     end
   end
 end
